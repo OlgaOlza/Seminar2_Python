e3427e813ce29bb66d1f0ff476d3078a55880aa2
OlgaOlza <olga-rgb06@rambler.ru>
2023-09-14 14:49:57 +0300

    Фибоначчи

---
+++ task_11.py
@@ -0,0 +1,25 @@
+"""
+Задача №11. Решение в группах
+Дано натуральное число A > 1. Определите, каким по
+счету числом Фибоначчи оно является, то есть
+выведите такое число n, что φ(n)=A. Если А не
+является числом Фибоначчи, выведите число -1.
+Input: 5
+Output: 6
+"""
+
+a = int(input('Введите число: '))
+if a == 0:
+    print(0)
+else:
+    fib_prev, fib_next = 0, 1
+    n = 2
+    while fib_next <= a:
+
+        if fib_next == a:
+            print(n)
+            break
+        fib_prev, fib_next = fib_next, fib_prev + fib_next
+        n += 1
+    else:
+        print(-1)
